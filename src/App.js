40fc858388af77641cc4938c2b701030ffefe1ea
AntonLan <lanqel@gmail.com>
2022-07-09 14:20:30 +0300

    add bootstrap grid container

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ import Profile from "./component/Profile";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <NavBar/>
-            <Profile/>
+        <div style={{minWidth: '100%', minHeight: '100%'}} className='container'>
+            <div className='row g-0'>
+                <div className='col' >
+                    <Header/>
+                </div>
+            </div>
+            <div className='row g-0'>
+                <div className='col-2'><NavBar/></div>
+                <div className='col-10'><Profile/></div>
+            </div>
         </div>
     );
 }
--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -4,30 +4,32 @@ import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className={s.content}>
-            <div>
-                <img
-                    src='https://mywowo.net/media/images/cache/dubai_img_worlds_of_adventure_01_presentazione_jpg_1200_630_cover_85.jpg'
+        <div className={`${s.content} conteiner`}>
+            <div className="row g-0">
+                <img style={{width: '900px'}} src='https://mywowo.net/media/images/cache/dubai_img_worlds_of_adventure_01_presentazione_jpg_1200_630_cover_85.jpg'
                     alt=''/>
             </div>
-            <div>
+
+            <div className={`${s.item} row g-0`}>
+                <img style={{width: '50px'}} src='https://ps.w.org/shortpixel-image-optimiser/assets/icon-256x256.png?rev=1038819' alt=''/>
                 ava + description
-                <img src='https://ps.w.org/shortpixel-image-optimiser/assets/icon-256x256.png?rev=1038819' alt=''/>
             </div>
-            <div>
+
+            <div className={`${s.item} row g-0`}>
                 my posts
-                <div>
+                <div className={`${s.item} row g-0`}>
                     New post
                 </div>
-                <div className={s.posts}>
-                    <div className={s.item}>
+                <div className={`${s.posts} row g-0`}>
+                    <div className={`${s.item} row`}>
                         post 1
                     </div>
-                    <div className={s.item}>
+                    <div className={`${s.item} row`}>
                         post 2
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
